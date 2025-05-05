f0f9f10e81b39264b94d23740c3f51568d537fe0
Hirosaki0_0 <128069058+Hirosaki4@users.noreply.github.com>
2025-05-05 11:56:57 +0300

    Create Завдання_1.py

---
+++ Завдання_1.py
@@ -0,0 +1,4 @@
+grades = [85, 60, 90, 70, 55, 100, 40, 78]
+high_grades = [grade for grade in grades if grade > 70]
+print("Оцінки вищі за 70:", high_grades)
+print("Кількість таких оцінок:", len(high_grades))
